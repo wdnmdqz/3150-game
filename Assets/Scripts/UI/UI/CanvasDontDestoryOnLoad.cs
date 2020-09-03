b148d413e0d43057fe615139bf8b8dc8d748ddf0
ChuhanZhou <chuhan.zhou1@students.mq.edu.au>
2020-09-03 17:23:12 +1000

    week5

---
+++ Assets/Scripts/UI/UI/CanvasDontDestoryOnLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasDontDestoryOnLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(1024,768,false);
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+}
